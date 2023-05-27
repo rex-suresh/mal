cd61f4af9b1105a36522b221a200fc3a97d8325d
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2023-05-27 16:23:33 +0530

    Added Step2 Eval

---
+++ impls/rex/step2_eval.js
@@ -0,0 +1,66 @@
+const readline = require('readline');
+const { stdin, stdout } = require('process');
+const { read_str } = require('./reader');
+const { pr_str } = require('./printer');
+const { MalSymbol, MalList, MalValue } = require('./types');
+const { MalVector } = require('./types');
+
+const rl = readline.createInterface({
+  input: stdin,
+  output: stdout
+});
+
+const repl_env = {
+  '+': (a, b) => new MalValue(a.value + b.value),
+  '*': (a, b) => new MalValue(a.value * b.value),
+  '-': (a, b) => new MalValue(a.value - b.value),
+  '/': (a, b) => new MalValue(a.value / b.value),
+};
+
+const eval_ast = (ast, env) => {
+  if (ast instanceof MalSymbol) {
+    return env[ast.value];
+  }
+
+  if (ast instanceof MalList) {
+    const newAST = ast.value.map(x => EVAL(x, env));
+    return new MalList(newAST);
+  }
+
+  if (ast instanceof MalVector) {
+    const newAST = ast.value.map(x => EVAL(x, env));
+    return new MalVector(newAST);
+  }
+
+  return ast;
+}
+
+const READ = (input) => read_str(input);
+const EVAL = (ast, env) => {
+  if (!(ast instanceof MalList)) {
+    return eval_ast(ast, env);
+  }
+
+  if (ast.isEmpty()) {
+    return ast;
+  }
+
+  const [fn, ...args] = eval_ast(ast, env).value;
+
+  return args.reduce(fn);
+};
+const PRINT = (malValue) => pr_str(malValue);
+const rep = (str) => PRINT(EVAL(READ(str), repl_env));
+
+const repl = () => {
+  rl.question('rex=> ', (line) => {
+    try {
+      console.log(rep(line));
+    } catch (error) {
+      console.log(error);
+    }
+    repl();
+  });
+}
+
+repl();
